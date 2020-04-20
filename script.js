b7fca10012d8f978bafb09e58bc158d5e1bfab6d
Glitch (hello-webpage) <none>
2020-04-20 14:56:41 +0000

    🐴🌺 Checkpoint ./script.js:15135430/390 ./style.css:15135430/93

--- script.js
+++ script.js
@@ -43,21 +43,25 @@ function draw_graph(canvas) {
 for (var i = 0; i < 2; i++) {
   var td_top = document.createElement('td');
   var table = document.createElement('table');
+  table.setAttribute("width",100);
+  
   var tr = document.createElement('tr');
   var th = document.createElement('th');
   var input = document.createElement('input');
   input.setAttribute("type","text");
   input.setAttribute("id", "title"+i);
   th.appendChild(input);
+  tr.appendChild(th);
+  table.appendChild(tr);
   
+  var tr = document.createElement('tr');
   var td = document.createElement('td');
   var canvas = document.createElement('canvas');
   canvas.setAttribute("id","canvas"+i);
-  th.appendChild(canvas);
-  
-  
-  tr.appendChild(th);
+  td.appendChild(canvas);
+  tr.appendChild(td);  
   table.appendChild(tr);
+  
   td_top.append(table);
   document.getElementById('data-tables').appendChild(td_top); 
   draw_graph(canvas);
