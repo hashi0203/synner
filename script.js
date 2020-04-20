5c674a9215e1287647c0fc414c0f1770fe5fc1f2
Glitch (hello-webpage) <none>
2020-04-20 13:57:41 +0000

    🖐☎️ Checkpoint ./script.js:15135430/906 ./index.html:15135430/63

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var td = document.createElement('td');
 for (var i = 0; i < 2; i++) {
+  var td_top = document.createElement('td');
   var table = document.createElement('table');
   var tr = document.createElement('tr');
+  var th = document.createElement('th');
+  var input = document.createElement('input');
+  input.setAttribute("type","text");
+  input.setAttribute("id", "title"+i);
+  th.appendChild(input);
+  tr.appendChild(th);
+  table.appendChild(tr);
+  td_top.append(table);
+  document.getElementById('data-tables').appendChild(td_top);
 }
 
-
-
-<td>
-          <table>
-            <tr>
-              <th><input type="text" name="title"></th>
-            </tr>
-          </table>
-        </td>
