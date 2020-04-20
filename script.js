8a4124bd7d2a8a8acebe396d61f93dd486aec4c2
Glitch (hello-webpage) <none>
2020-04-20 14:32:41 +0000

    👳🙎 Checkpoint ./script.js:15135430/3439

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function draw_graph(canvas) {
+  //「月別データ」
+  var mydata = {
+    labels: ["１月", "２月", "３月", "４月", "５月", "６月", "７月"],
+    datasets: [
+      {
+        label: '数量',
+        hoverBackgroundColor: "rgba(255,99,132,0.3)",
+        data: [65, 59, 80, 81, 56, 55, 48],
+      }
+    ]
+  };
+
+  //「オプション設定」
+  var options = {
+    legend: {
+        display: false
+     }
+  };
+
+  var chart = new Chart(canvas, {
+
+    type: 'bar',  //グラフの種類
+    data: mydata,  //表示するデータ
+    // options: options  //オプション設定
+
+  });
+};
+
+
 for (var i = 0; i < 2; i++) {
   var td_top = document.createElement('td');
   var table = document.createElement('table');
@@ -15,35 +45,7 @@ for (var i = 0; i < 2; i++) {
   tr.appendChild(th);
   table.appendChild(tr);
   td_top.append(table);
-  document.getElementById('data-tables').appendChild(td_top);
-  
-  //「月別データ」
-  var mydata = {
-    labels: ["１月", "２月", "３月", "４月", "５月", "６月", "７月"],
-    datasets: [
-      {
-        label: '数量',
-        hoverBackgroundColor: "rgba(255,99,132,0.3)",
-        data: [65, 59, 80, 81, 56, 55, 48],
-      }
-    ]
-  };
-
-  //「オプション設定」
-  var options = {
-    title: {    
-      display: true,
-      text: 'サンプルチャート'
-    }
-  };
-
-  var chart = new Chart(canvas, {
-
-    type: 'bar',  //グラフの種類
-    data: mydata,  //表示するデータ
-    options: options  //オプション設定
-
-  });
-  // <canvas id="stage"></canvas>
+  document.getElementById('data-tables').appendChild(td_top); 
+  draw_graph(canvas);
 }
 
