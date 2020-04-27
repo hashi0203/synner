3cbcb9e7b6a1320ab2856413f7068a0528bbbfcb
Glitch (synner) <none>
2020-04-27 08:55:29 +0000

    👁🍕 Checkpoint ./script.js:15135430/348 ./style.css:15135430/393

--- script.js
+++ script.js
@@ -237,10 +237,13 @@ function make_data_detail_content() {
   app_child([th,tr1]);
   var th = new_elem('th');
   th.textContent = 'Distribution';
-  app_child([th,tr1,table1]);
-  var tr1 = new_elem('tr');
+  app_child([th,tr1,table1,td]);
+  var div = new_elem('div');
+  add_atts(div,[['class','y_scroll']]);
+  var table1 = new_elem('table');
   var data = toCountDict(json[data_idx]['data']);
   for (var i = 0; i < data[0].length; i++) {
+    var tr1 = new_elem('tr');
     var td1 = new_elem('td');
     var input = new_elem('input');
     add_atts(input,[['type','text'],['class','size_fix'], ['id','value'+i],['value',data[0][i]]]);
@@ -250,7 +253,7 @@ function make_data_detail_content() {
     add_atts(input,[['type','text'],['class','size_fix'], ['id','dist'+i],['value',data[1][i]*100/data_number]]);
     app_child([input,td1,tr1,table1]);
   }
-  app_child([table1,td]);
+  app_child([table1,div,td]);
   
   
   app_child([td,tr,table,document.getElementById('data_detail_content')]);
